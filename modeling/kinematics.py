0b4f9c70725cee240be6877c2321e79c8479ef38
Cody Hyman <hymanc@umich.edu>
2014-11-24 11:52:56 -0500

    Initial commit of kinematics stubs

---
+++ modeling/kinematics.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+
+class PlanarArm:
+    
+    def __init__():
+        
+            
+
+def forwardKinematics(arm, configuration):
+    # Return end effector position
+
+
+def inverseKinematics(arm, target):
+    # Return joint variables
